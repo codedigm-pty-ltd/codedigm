d49de681c3b24e83295fc976bff0a970fbfeb84e
Jacques <jacques.duplessis@codedigm.co.za>
2019-11-23 06:15:34 +0200

    Updates

--- contactus.php
+++ contactus.php
@@ -11,8 +11,8 @@ $mail->Host = 'mail.codedigm.co.za'; // Specify main and backup SMTP servers
 $mail->SMTPAuth = true; // Enable SMTP authentication
 $mail->Username = 'info@codedigm.co.za'; // SMTP username
 $mail->Password = 'C0ded5Gm@1'; // SMTP password
-$mail->SMTPSecure = 'tls'; // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 587;
+//$mail->SMTPSecure = 'tls'; // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 25;
 $mail->isHTML(true);
 
 try {
